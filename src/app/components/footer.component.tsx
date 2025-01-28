08cf064927d8d145d80161ab537b88dd2e8ae5d2
Axel Junior <axeljunior.dev@gmail.com>
2025-01-28 20:06:16 -0300

    frist commit

---
+++ src/app/components/footer.component.tsx
@@ -0,0 +1,10 @@
+export async function FooterComponent() {
+    return (
+        <footer className="h-[150px] bg-zinc-900">
+            <div title="MenuButtons" className="flex h-full">
+                <div title="Contato" className="mt-4 ml-4 basis-1/5 min-w-[200px]">CONTATO</div>
+                <div title="Creditos" className="mt-4 ml-4 basis-4/5">CREDITOS</div>
+            </div>
+        </footer>
+    )
+}
---
+++ src/app/components/header.component.tsx
@@ -0,0 +1,16 @@
+
+export async function HeaderComponent() {
+    return (
+        <nav className="flex h-[80px] w-full sticky top-0 bg-zinc-900">
+            <div title="MenuButtons" className="flex basis-11/12">
+                <button title="Home" className=" bg-red-950 w-[180px]">HOME</button>
+                <button title="Mapa" className=" bg-amber-950 w-[180px]">MAPA</button>
+            </div>
+            <div title="LoginButton" className="basis-1/12">
+                <div title="LoginWrapper" className=" flex items-center justify-center bg-teal-950 size-full min-w-[100px]">
+                    <button className="bg-black size-[70px] rounded-full">LOGIN</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
