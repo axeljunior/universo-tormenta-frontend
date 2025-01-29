8bcd061d2d97b6de74e6d6c9d1b29cd9c449e63c
Axel Junior <axeljunior.dev@gmail.com>
2025-01-29 20:22:09 -0300

    Atualiza tailwind para v4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 export default function Home() {
 
   return (
-    <div className="w-97 h-97 bg-black">
-      <header className="w-2512 min-h-screen bg-blue-950 bg-[url(../public/homeTopImage.jpg)] bg-cover bg-center">
-          HEADER
-      </header>
+    <div className="w-[97] h-[97] bg-black">
+      <header className="w-[2512] min-h-screen bg-blue-950 bg-[url(../public/homeTopImage.jpg)] bg-cover bg-center"></header>
       <main className="flex flex-col items-center w-full bg-[url(../public/tormentaEye-horizontal.jpg)] bg-repeat-x bg-top">
           <section title="Cards" className="min-h-[89vh]"></section>
           <section title="Slide Deuses" className="min-h-[75vh] min-w-[1040px] bg-yellow-900">Slide Deuses</section>
